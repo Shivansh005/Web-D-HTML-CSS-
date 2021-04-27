13bdd244e43ba1eea3c17c053dafd334608aa72d
Shivansh005 <68140375+Shivansh005@users.noreply.github.com>
2021-04-27 10:31:44 +0530

    Add files via upload
    scripts added in the markup

---
+++ tut49.js
@@ -0,0 +1,30 @@
+console.log("This is tutorial 49");
+
+function greet(name, time){
+    console.log(name + ' is a good boy');
+    console.log(time +' is running');
+}
+
+function sum(a,b,c)
+{
+    let d=a+b+c;
+    return d;
+}
+let name="Harry";
+let name1="Harry1";
+let name2="Harry2";
+let name3="Harry5";
+
+let time1="10:45";
+let time2="23:45";
+let time3="12:45";
+let time4="11:45";
+// console.log(name + " is a goog boy");
+
+greet(name,time1);
+greet(name1,time2);
+greet(name2,time3);
+greet(name3,time4);
+
+let summation=sum(5,6,7);
+console.log(summation);
